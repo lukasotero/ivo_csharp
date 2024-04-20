cdf1a4c4b2b7d70372dbfb1d013daa6dfe1d01ff
lukasotero <lukasn.otero@gmail.com>
2024-04-20 17:08:58 -0300

    Add project files.

---
+++ Ejercicio1/Cuenta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    public class Cuenta
+    {
+        // Atributos
+        private int saldo;
+
+        // Constructor
+        public Cuenta(int saldo)
+        {
+            this.saldo = saldo;
+        }
+
+        // Métodos
+        public void Acreditar(int saldo)
+        {
+            if (saldo > 0)
+            {
+                this.saldo += saldo;
+            } else
+            {
+                Console.WriteLine("El monto a acreditar no puede ser menor que 0.");
+            }
+        }
+
+        // Getter
+        public int GetSaldo()
+        { 
+            return saldo; 
+        }
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    public class Program
+    {
+        /* 1) Realizar un programa que dada una clase "Cuenta",
+         * cuyo atributo es el saldo de la misma permita 'Acreditar' saldo sobre la misma.
+         * El valor que se acredita no puede ser menor que 0.
+
+        Debe incluir un método constructor que permita inicializar el valor inicial del saldo.
+
+        Crear los objetos necesarios para realizar las pruebas con los llamados a los métodos correspondientes.
+        */
+
+        static void Main(string[] args)
+        {
+            Cuenta cuenta = new Cuenta(0);
+            Console.WriteLine("Saldo antes: " + cuenta.GetSaldo());
+
+            cuenta.Acreditar(100);
+
+            Console.WriteLine("Saldo después: " + cuenta.GetSaldo());
+            Console.Read();
+        }
+    }
+}
